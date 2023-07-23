d8809c4247446b2006c8659a5a54c5e98da0bc58
jaked <jdeford199@gmail.com>
2023-07-23 16:44:24 -0400

    Reverted some changes to the resume iframe.

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import './resume.css';
-
+// import './resume.css';
 const resumePDF = process.env.PUBLIC_URL + '/resume.pdf';
 
 function Resume() {
@@ -12,3 +11,27 @@ function Resume() {
 }
 
 export default Resume;
+
+// import React from 'react';
+// import './resume.css'; // Import the CSS file
+// const resumePDF = process.env.PUBLIC_URL + '/resume.pdf';
+
+
+// function Resume() {
+//   return (
+//     <div>
+//       <div className="resizable-box">
+//         <div className="iframe-container">
+//           <iframe
+//             title="Resume"
+//             src={resumePDF}
+//             frameBorder="0"
+//             allowFullScreen
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Resume;
