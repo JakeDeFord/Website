08652ecf91b5710e2323eb9339c805a04eaf7219
jacobd <jacobd@mastodondesign.com>
2023-07-25 14:46:37 -0400

    Changed to google drive embedded link for resume pdf

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './resume.css';
+
 const resumePDF = process.env.PUBLIC_URL + '/resume.pdf';
 
 function Resume() {
   return (
     <div className="resume-container">
-      <iframe src={resumePDF} className="resume-pdf" title='Jake DeFord Resume' />
+      <iframe src="https://drive.google.com/file/d/1aFl2WHnCx_T01kS6Iy6bWpFARwaN5Dgw/preview" width="640" height="480" allow="autoplay"></iframe>
     </div>
   );
 }
