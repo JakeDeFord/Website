a6826fcff1859f56fa45edf47240383a8618f4d9
jacobd <jacobd@mastodondesign.com>
2023-07-25 15:06:37 -0400

    Changed height for resume and removed padding

--- src/components/resume.js
+++ src/components/resume.js
@@ -6,7 +6,7 @@ const resumePDF = process.env.PUBLIC_URL + '/resume.pdf';
 function Resume() {
   return (
     <div className="resume-container">
-      <iframe src="https://drive.google.com/file/d/1aFl2WHnCx_T01kS6Iy6bWpFARwaN5Dgw/preview" width="640" height="480" allow="autoplay"></iframe>
+      <iframe src="https://drive.google.com/file/d/1aFl2WHnCx_T01kS6Iy6bWpFARwaN5Dgw/preview" width="600" height="780" allow="autoplay" ></iframe>
     </div>
   );
 }
