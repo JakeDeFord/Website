fbfa9e94814c1814c4ecb990beaabd8ab8eaa10c
jacobd <jacobd@mastodondesign.com>
2023-09-01 11:53:18 -0400

    Added project 1 description and css for formatting

--- src/projects/project1.js
+++ src/projects/project1.js
@@ -1,16 +1,20 @@
 // Project1.js
 import React from 'react';
 import Project1_pic from '../images/dancing_groot.gif';
+import './project1.css'; // Import the CSS file
 
 function Project1() {
     return (
-        <div className="project-content"> {/* Apply the project-content class */}
+        <div className="project-content">
             <h3>Dancing Groot</h3>
-            <div className="project-image-container"> {/* Add a container for the image */}
+            <div className="project-image-container">
                 <img src={Project1_pic} alt="Dancing Groot" />
             </div>
-            <p>Description of Project 1</p>
-            {/* Add any other details or images related to Project 1 */}
+            <p>
+                Purchased a Dancing Groot from Amazon and was not pleased with its functions. The songs programmed into him were not authentic and were knockoffs.
+                I opened the toy up and replaced the internal microcontroller and installed my own. I used a nuvaton microcontroller that had a speaker controller and LED controller already built in.
+                I then ripped some youtube videos for music from the movies Groot was in and individualy edited the mp3 files to improve their performance for a toy speaker.
+            </p>
         </div>
     );
 }
