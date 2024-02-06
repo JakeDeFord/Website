b7e37eef63b6afeeb53f6c68f447bd0f66b563a3
JakeDeFord <jdeford199@gmail.com>
2024-02-06 18:43:06 -0500

    Added bold text to github hyperlink. Also added github hyperlink to project in projects

--- src/projects/project3.js
+++ src/projects/project3.js
@@ -1,4 +1,4 @@
-// Project1.js
+// Project3.js
 import React from 'react';
 import Project3_pic from '../images/communication_instrument.jpg';
 import './project3.css'; // Import the CSS file
@@ -6,10 +6,11 @@ import './project3.css'; // Import the CSS file
 function Project3() {
     return (
         <div className="project-content"> {/* Apply the project-content class */}
-            <h3>Communication Instrument</h3>
+            <h3>Custom Ardiono Project</h3>
             <div className="project-image-container">
                 <img src={Project3_pic} alt="Communication Instrument" />
             </div>
+            <a href="https://github.com/JakeDeFord/Arduino_Project" target="_blank" rel="noopener noreferrer"><strong>Arduino Project source code on GitHub</strong></a>
             <p>
                 Devised and assessed a budget-friendly alternative to a costly commercial product. This device included an Arduino Due, an ethernet shield, and a proto shield.
                 This device was created to be a drop-in instrument that can communicate with hardware using most protocols. These protocols include SPI, I2c, UART, GPIO, and PWM.
