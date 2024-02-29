185e34594aecc7f6febae9eb75cc9aabd6dbc687
jacobd <jacobd@mastodondesign.com>
2024-02-29 09:58:35 -0500

    Changed animation to video

--- src/components/background.js
+++ src/components/background.js
@@ -1,12 +1,15 @@
 // background.js
 import React from 'react';
-import ParticleBackground from './particle_background'; // Import ParticleBackground component
 import './background.css'; // Add your background container styles here
+import background_video from "../videos/background.mp4"
 
 const BackgroundContainer = ({ children }) => {
     return (
         <div className="background">
-            <ParticleBackground /> {/* Render ParticleBackground component */}
+            <video autoPlay loop muted className="background-video">
+                <source src={background_video} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
             {children}
         </div>
     );
