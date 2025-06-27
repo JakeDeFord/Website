b763446c1b458fd591fbdf7720df0af7d7b1f353
jaked <jdeford199@gmail.com>
2025-06-27 17:06:14 -0400

    refactor: re-removed Page from page folders, and fixed url for resume again

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './global.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/Home/Home.js';
-import AboutPage from './pages/AboutPage/AboutPage.js';
-import ProjectsPage from './pages/ProjectsPage/ProjectsPage.js';
-import ContactPage from './pages/ContactPage/ContactPage.js';
-import ResumePage from './pages/ResumePage/ResumePage.js';
-import Navbar from './components/navbar'; // Path remains the same
-import Footer from './components/footer'; // Path remains the same
-import Background from './components/background'; // Path remains the same
+import React from "react";
+import "./global.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Home/Home.js";
+import AboutPage from "./pages/About/AboutPage.js";
+import ProjectsPage from "./pages/Projects/ProjectsPage.js";
+import ContactPage from "./pages/Contact/ContactPage.js";
+import ResumePage from "./pages/Resume/ResumePage.js";
+import Navbar from "./components/navbar"; // Path remains the same
+import Footer from "./components/footer"; // Path remains the same
+import Background from "./components/background"; // Path remains the same
 
 function App() {
   return (
