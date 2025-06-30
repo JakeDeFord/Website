30b8ccbadc3a8ae793d567c197ba4a6c6f21e686
jaked <jdeford199@gmail.com>
2025-06-30 17:14:25 -0400

    fix: removed some of the jules changes

--- src/components/background.js
+++ src/components/background.js
@@ -1,16 +1,16 @@
 // background.js
-import React from 'react';
-import './background.css'; // Add your background container styles here
+import React from "react";
+import "./background.css"; // Add your background container styles here
 // Video import removed as video is no longer used
 // import background_video from "../videos/background.mp4"
 
 const BackgroundContainer = ({ children }) => {
-    return (
-        <div className="background">
-            {/* Video element removed */}
-            {children}
-        </div>
-    );
-}
+  return (
+    <main id="main-content" className="background">
+      {/* Video element removed */}
+      {children}
+    </main>
+  );
+};
 
 export default BackgroundContainer;
